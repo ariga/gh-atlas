5642fc55e9147f40dcfca57e29c1b167f7757dc5
Ronen Lubin <ronen7822@gmail.com>
2023-05-02 21:37:04 +0300

    add intgeration test in main

--- main_test.go
+++ main_test.go
@@ -10,7 +10,7 @@ import (
 
 func TestIntegration(t *testing.T) {
 	if os.Getenv("GH_ATLAS_INTEGRATION") != "1" {
-		t.Skipf("skipping integration test, run with env INTEGRATION=1")
+		t.Skipf("skipping integration test, run with env GH_ATLAS_INTEGRATION=1")
 	}
 	err := exec.Command("gh", "atlas", "init-ci", "-t", os.Getenv("ATLAS_CLOUD_TOKEN"), "--driver=mysql", "testdata/migrations").Run()
 	require.NoError(t, err)
