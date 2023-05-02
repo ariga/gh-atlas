be198895bf27311eb6d1491e5fe953ffd2521e89
Ronen Lubin <ronen7822@gmail.com>
2023-05-02 21:32:14 +0300

    create integration test workflow

---
+++ main_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestIntegration(t *testing.T) {
+	if os.Getenv("GH_ATLAS_INTEGRATION") != "1" {
+		t.Skipf("skipping integration test, run with env INTEGRATION=1")
+	}
+	err := exec.Command("gh", "atlas", "init-ci", "-t", os.Getenv("ATLAS_CLOUD_TOKEN"), "--driver=mysql", "testdata/migrations").Run()
+	require.NoError(t, err)
+}
